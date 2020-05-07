54a8bd9625114115695db839274b7849c2ba50b2
Jayneet Porwal <porwaljayneet97@gmail.com>
2020-05-07 16:35:08 +0530

    added authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public afAuth:AngularFireAuth,public db:AngularFirestore,public router:Router) {
+    this.afAuth.authState.subscribe(res=>{
+      if(res){
+        localStorage.setItem("uid",res.uid)
+      }
+      else{
+        localStorage.removeItem("uid")
+        this.router.navigateByUrl("/auth")
+      }
+    })
+   }
+
+   createAccount(cred:{email:string,password:string},profileInfo){
+     return this.afAuth.createUserWithEmailAndPassword(cred.email,cred.password).then(res=>{
+       localStorage.setItem("uid",res.user.uid)
+       return this.db.collection("users").doc(res.user.uid).set(Object.assign({},profileInfo))
+     }).catch(err=>{
+       // code to generate a notification alert of wrong credentials
+       alert(err)
+       return err
+     })
+   }
+
+   signIn(email,password){
+     return this.afAuth.signInWithEmailAndPassword(email,password).then(res=>{
+      localStorage.setItem("uid",res.user.uid)
+      return res.user.uid
+     }).catch(err=>{
+      // code to generate a notification alert of wrong credentials
+      alert(err)
+      return err
+    })
+   }
+
+   resetPassword(email){
+    return this.afAuth.sendPasswordResetEmail(email)
+   }
+
+   isAuthenticated(){
+    if(localStorage.getItem("uid")){
+      return true
+    }
+    else{
+      return false
+    }
+   }
+
+   logOut(){
+      localStorage.removeItem("uid")
+      this.afAuth.signOut()
+      window.location.reload()
+   }
+
+
 }
