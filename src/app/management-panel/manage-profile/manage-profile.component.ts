1876149c16953f4e7a03fa0fb7af1e380820d216
pradeepbhardwaj081 <pradeepbhardwaj081@gmail.com>
2020-05-09 00:18:12 +0530

    pradeep:profile

--- src/app/management-panel/manage-profile/manage-profile.component.ts
+++ src/app/management-panel/manage-profile/manage-profile.component.ts
@@ -7,11 +7,18 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./manage-profile.component.scss']
 })
 export class ManageProfileComponent implements OnInit {
-
+  profileInfo:any
   constructor( private authService: AuthService ) { }
 
   ngOnInit(): void {
-    console.log(this.authService.getProfile())
+  this.authService.getProfile().subscribe(res=>{
+    this.profileInfo=res
+    console.log(this.profileInfo)
+  })
+  }
+
+  update(formData){
+    this.authService.updateProfile(formData.value)
   }
 
 }
